9c874787048f9fb1250993410af5de8647645eba
Juan Pablo Garcia Diez <juanp.garciad@upb.edu.co>
2019-02-18 00:32:21 -0500

    Inicio app

---
+++ SMove/SMove/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms.Xaml;
+
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace SMove
+{
+    using SMove.Views;
+    using Xamarin.Forms;
+
+    public partial class App : Application
+    {
+
+
+        #region Constructor
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new LoginPage();
+        }
+        #endregion
+
+        #region Metodos
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
+}
