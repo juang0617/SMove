1e1381f5d76bed97572fc49263460739ecc03486
Juan Pablo Garcia Diez <juanp.garciad@upb.edu.co>
2019-03-24 23:56:26 -0500

    Backend 2

---
+++ SMove.Backend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SMove.Backend.Startup))]
+namespace SMove.Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
