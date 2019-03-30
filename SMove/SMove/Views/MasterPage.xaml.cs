cb39331707a676128e4850e2125d807cefd8d04d
Juan Pablo Garcia Diez <juanp.garciad@upb.edu.co>
2019-03-30 16:21:10 -0500

    Configuraciones iniciales master page

--- SMove/SMove/Views/MasterPage.xaml.cs
+++ SMove/SMove/Views/MasterPage.xaml.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-namespace SMove.Views
+﻿namespace SMove.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MasterPage : ContentPage
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MasterPage : MasterDetailPage
 	{
 		public MasterPage ()
 		{
