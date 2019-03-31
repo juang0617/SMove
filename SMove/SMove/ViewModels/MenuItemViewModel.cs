d05ffd650a08b7c496897f0b648ed927096e949c
Juan Pablo Garcia Diez <juanp.garciad@upb.edu.co>
2019-03-30 23:43:20 -0500

    Cerrar sesion

--- SMove/SMove/ViewModels/MenuItemViewModel.cs
+++ SMove/SMove/ViewModels/MenuItemViewModel.cs
@@ -1,13 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SMove.ViewModels
+﻿namespace SMove.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using SMove.Views;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
     public class MenuItemViewModel
     {
+        #region Propiedades
         public string Icon { get; set; }
         public string Title { get; set; }
-        public string PageName { get; set; }
+        public string PageName { get; set; } 
+        #endregion
+
+        #region Comandos
+        public ICommand NavigateCommand
+        {
+            get
+            {
+                return new RelayCommand(Navigate);
+            }
+        }
+
+        private void Navigate()
+        {
+            if (this.PageName == "EnterPage")
+            {
+                Application.Current.MainPage = new EnterPage();
+            }
+        }
+        #endregion
     }
 }
