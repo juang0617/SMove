a997a8ad72b6a4d20820ad04c6195bd13426b5cb
Juan Pablo Garcia Diez <juanp.garciad@upb.edu.co>
2019-04-07 17:21:06 -0500

    Update MainActivity.cs

--- SMove/SMove.Android/MainActivity.cs
+++ SMove/SMove.Android/MainActivity.cs
@@ -6,21 +6,35 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Firebase;
 
 namespace SMove.Droid
 {
     [Activity(Label = "SMove", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public static FirebaseApp app;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+            InitFirebaseAuth();
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             global::Xamarin.FormsMaps.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+           
+        }
+
+        private void InitFirebaseAuth()
+        {
+            var options = new FirebaseOptions.Builder().SetApplicationId("1:745578531830:android:4d9d31a6b1d50461").SetApiKey("AIzaSyBGpZDTgASgMdr1e2jAud2sSxYuDYLMLAA").SetDatabaseUrl("https://smove-1552199852284.firebaseio.com/").Build();
+
+            if (app == null)
+                app = FirebaseApp.InitializeApp(this, options, "SMove");
         }
     }
 }
