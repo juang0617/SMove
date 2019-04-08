c93a4ed91903acafc03b88a6281a874e681f773b
Juan Pablo Garcia Diez <juanp.garciad@upb.edu.co>
2019-04-07 21:48:20 -0500

    Update FirebaseAuthService.cs

--- SMove/SMove.Android/FirebaseAuthService.cs
+++ SMove/SMove.Android/FirebaseAuthService.cs
@@ -1,18 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-
+﻿
 namespace SMove.Droid
 {
-    class FirebaseAuthService
+    using System;
+    using System.Threading.Tasks;
+    using Android.App;
+    using Android.Content;
+    using Firebase.Auth;
+    using SMove.Services;
+    using Xamarin.Forms;
+
+    public class FirebaseAuthService : IFirebaseAuthService
     {
+        public static int REQ_AUTH = 9999;
+        public static string KEY_AUTH = "auth";
+
+        public string getAuthKey()
+        {
+            return KEY_AUTH;
+        }
+
+        public bool IsUserSigned()
+        {
+            var user = Firebase.Auth.FirebaseAuth.GetInstance(MainActivity.app).CurrentUser;
+            var signedIn = user != null;
+            return signedIn;
+        }
+
+        public async Task<bool> Logout()
+        {
+            try
+            {
+                Firebase.Auth.FirebaseAuth.GetInstance(MainActivity.app).SignOut();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> SignIn(string email, string password)
+        {
+            try
+            {
+                await Firebase.Auth.FirebaseAuth.GetInstance(MainActivity.app).SignInWithEmailAndPasswordAsync(email, password);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public void SignInWithGoogle()
+        {
+            var googleIntent = new Intent(Forms.Context, typeof(GoogleLoginActivity));
+            ((Activity)Forms.Context).StartActivityForResult(googleIntent, REQ_AUTH);
+        }
+
+        public async Task<bool> SignInWithGoogle(string token)
+        {
+            try
+            {
+                AuthCredential credential = GoogleAuthProvider.GetCredential(token, null);
+                await Firebase.Auth.FirebaseAuth.GetInstance(MainActivity.app).SignInWithCredentialAsync(credential);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> SignUp(string email, string password)
+        {
+            try
+            {
+                await Firebase.Auth.FirebaseAuth.GetInstance(MainActivity.app).CreateUserWithEmailAndPasswordAsync(email, password);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public string GetUserId()
+        {
+            var user = Firebase.Auth.FirebaseAuth.GetInstance
+
+            (MainActivity.app).CurrentUser;
+            return user.Uid;
+        }
     }
+}
 }
