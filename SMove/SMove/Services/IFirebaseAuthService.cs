533d04f5673b24c54aaa939e7fdd88488e359ea3
Juan Pablo Garcia Diez <juanp.garciad@upb.edu.co>
2019-04-07 21:43:00 -0500

    Create IFirebaseAuthService.cs

---
+++ SMove/SMove/Services/IFirebaseAuthService.cs
@@ -0,0 +1,16 @@
+﻿namespace SMove.Services
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public interface IFirebaseAuthService
+    {
+        String getAuthKey();
+        bool IsUserSigned();
+        Task<bool> SignUp(String email, String password);
+        Task<bool> SignIn(String email, String password);
+        void SignInWithGoogle();
+        Task<bool> SignInWithGoogle(String token);
+        Task<bool> Logout();
+    }
+}
