1734321463e8194e3b4e96c5cdf45a20632fdf65
Juan Pablo Garcia Diez <juanp.garciad@upb.edu.co>
2019-04-22 00:14:42 -0500

    Login

--- SMove/SMove/ViewModels/LoginViewModel.cs
+++ SMove/SMove/ViewModels/LoginViewModel.cs
@@ -8,10 +8,13 @@ namespace SMove.ViewModels
     using System.Windows.Input;
     using Xamarin.Forms;
     using Views;
+    using Services;
 
     public class LoginViewModel : BaseViewModel
     {
-
+        #region Servicios
+        private ApiService apiService;
+        #endregion
 
         #region Atributos
         private string email;
@@ -48,6 +51,7 @@ namespace SMove.ViewModels
         #region Constructores
         public LoginViewModel()
         {
+            this.apiService = new ApiService();
             this.IsRemembered = true;
             this.IsEnabled = true;
         }
@@ -74,29 +78,57 @@ namespace SMove.ViewModels
             if (string.IsNullOrEmpty(this.Email))
             {
                 await Application.Current.MainPage.DisplayAlert("Error", "Debe ingresar un correo.", "Aceptar");
+                return;
             }
             if (string.IsNullOrEmpty(this.Password))
             {
                 await Application.Current.MainPage.DisplayAlert("Error", "Debe ingresar una contraseña.", "Aceptar");
+                return;
             }
 
             this.IsRunning = true;
             this.IsEnabled = false;
 
-            if (this.Email != "juan0617@hotmail.com" || this.Password != "1234")
+            var connection = await this.apiService.CheckConnection();
+            if (!connection.IsSuccess)
             {
                 this.IsRunning = false;
                 this.IsEnabled = true;
-                await Application.Current.MainPage.DisplayAlert("Error", "Correo o contraseñas incorrectos.", "Aceptar");
-                this.Password = string.Empty;
+                await Application.Current.MainPage.DisplayAlert("Error", connection.Message, "Aceptar");
+                return;
             }
+
+            var token = await this.apiService.GetToken("https://smoveapi.azurewebsites.net", this.Email, this.Password);
+
+            if (token == null)
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert("Error", "Algo fallo, intente de nuevo", "Aceptar");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(token.AccessToken))
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert("Error", token.ErrorDescription, "Aceptar");
+                this.Password = string.Empty;
+                return;
+            }
+
+            var mainViewModel = MainViewModel.GetInstance();
+            mainViewModel.Token = token;
+
+            Application.Current.MainPage = new MasterPage();
 
             this.IsRunning = false;
             this.IsEnabled = true;
+
             this.Email = string.Empty;
             this.Password = string.Empty;
 
-            Application.Current.MainPage = new MasterPage();
+            
 
         }
 
