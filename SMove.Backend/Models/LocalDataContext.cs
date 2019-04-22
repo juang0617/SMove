b1de59b5776594871e007c98b0c4d59d7a7ecb5a
Juan Pablo Garcia Diez <juanp.garciad@upb.edu.co>
2019-04-22 13:50:56 -0500

    Controlador Usuario

---
+++ SMove.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,9 @@
+﻿namespace SMove.Backend.Models
+{
+    using Domain;
+
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<SMove.Domain.User> Users { get; set; }
+    }
+}
