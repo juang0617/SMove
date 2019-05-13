01386051794eea8186eb1a0f029bde3d514f1618
Juan Pablo Garcia Diez <juanp.garciad@upb.edu.co>
2019-05-13 14:03:31 -0500

    Create UserView.cs

---
+++ SMove.Backend/Models/UserView.cs
@@ -0,0 +1,21 @@
+﻿namespace SMove.Backend.Models
+{
+    using Domain;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [NotMapped]
+    public class UserView : User
+    {
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "El campo {0} es necesario.")]
+        [StringLength(20, ErrorMessage = "El campo {0} debe contener entre {1} y {2} caracteres", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "El campo {0} es necesario.")]
+        [Compare("Password", ErrorMessage = "La contraseña y su confirmación no coinciden")]
+        [Display(Name = "Password confirm")]
+        public string PasswordConfirm { get; set; }
+    }
+}
