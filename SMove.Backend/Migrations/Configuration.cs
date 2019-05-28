017e8604b82c3690ff1a6ce38652f18f283ef870
Juan Pablo Garcia Diez <juanp.garciad@upb.edu.co>
2019-05-27 19:36:26 -0500

    Cambios al UserController

---
+++ SMove.Backend/Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+namespace SMove.Backend.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SMove.Backend.Models.LocalDataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "SMove.Backend.Models.LocalDataContext";
+        }
+
+        protected override void Seed(SMove.Backend.Models.LocalDataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
