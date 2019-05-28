ebab40fdd2407a56b68f315de2d99371a1ce558b
Juan Pablo Garcia Diez <juanp.garciad@upb.edu.co>
2019-05-27 19:35:59 -0500

    Tipos de usuario

--- SMove.Backend/Models/LocalDataContext.cs
+++ SMove.Backend/Models/LocalDataContext.cs
@@ -5,5 +5,7 @@
     public class LocalDataContext : DataContext
     {
         public System.Data.Entity.DbSet<SMove.Domain.User> Users { get; set; }
+
+        public System.Data.Entity.DbSet<SMove.Domain.UserType> UserTypes { get; set; }
     }
 }
---
+++ SMove.Domain/UserType.cs
@@ -0,0 +1,22 @@
+﻿namespace SMove.Domain
+{
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class UserType
+    {
+        [Key]
+        public int UserTypeId { get; set; }
+
+
+        [Required(ErrorMessage = "El campo {0} es necesario.")]
+        [MaxLength(50, ErrorMessage = "El campo {0} solo puede contener un maximo de {1} caracteres.")]
+        [Index("UserType_Name_Index", IsUnique = true)]
+        public string Name { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
