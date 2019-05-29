85017a37c0b9f0509aca44a646a85a4b95c03fc3
Juan Pablo Garcia Diez <juanp.garciad@upb.edu.co>
2019-05-29 02:23:39 -0500

    Cambio modelo registro

--- SMove/SMove/ViewModels/RegisterViewModel.cs
+++ SMove/SMove/ViewModels/RegisterViewModel.cs
@@ -1,65 +1,98 @@
 ﻿namespace SMove.ViewModels
 {
-    using GalaSoft.MvvmLight.Command;
-    using System;
-    using System.ComponentModel;
     using System.Windows.Input;
-    using Xamarin.Forms;
+    using Domain;
+    using GalaSoft.MvvmLight.Command;
+    using Helpers;
+    using Plugin.Media;
+    using Plugin.Media.Abstractions;
+    using Services;
     using Views;
+    using Xamarin.Forms;
 
     public class RegisterViewModel : BaseViewModel
     {
-        #region Atributos
-        private string email;
-        private string password;
-        private string nombre;
-        private string apellidos;
-        private bool isRunning;
-        private bool isEnabled;
+        #region Services
+        private ApiService apiService;
         #endregion
 
-        #region Propiedades
-        public string Email
+        #region Attributes
+        private bool isRunning;
+        private bool isEnabled;
+        private ImageSource imageSource;
+        private MediaFile file;
+        #endregion
+
+        #region Properties
+        public ImageSource ImageSource
         {
-            get { return this.email; }
-            set { SetValue(ref this.email, value); }
+            get { return this.imageSource; }
+            set { SetValue(ref this.imageSource, value); }
         }
-        public string Password
-        {
-            get { return this.password; }
-            set { SetValue(ref this.password, value); }
-        }
-        public string Nombre
-        {
-            get { return this.nombre; }
-            set { SetValue(ref this.nombre, value); }
-        }
-        public string Apellidos
-        {
-            get { return this.apellidos; }
-            set { SetValue(ref this.apellidos, value); }
-        }
-        public bool IsRunning
-        {
-            get { return this.isRunning; }
-            set { SetValue(ref this.isRunning, value); }
-        }
-        public bool IsRemembered { get; set; }
+
         public bool IsEnabled
         {
             get { return this.isEnabled; }
             set { SetValue(ref this.isEnabled, value); }
         }
-        #endregion
 
-        #region Constructores
-        public RegisterViewModel()
+        public bool IsRunning
         {
-            this.IsRemembered = true;
-            this.IsEnabled = true;
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
+
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string Telephone
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        public string Confirm
+        {
+            get;
+            set;
         }
         #endregion
-        #region Comandos
+
+        #region Constructors
+        public RegisterViewModel()
+        {
+            this.apiService = new ApiService();
+
+            this.IsEnabled = true;
+            this.ImageSource = "no_image";
+        }
+        #endregion
+
+        #region Methods
+        #endregion 
+
+        #region Commands
         public ICommand RegisterCommand
         {
             get
@@ -68,18 +101,216 @@
             }
         }
 
+        private async void Register()
+        {
+            if (string.IsNullOrEmpty(this.FirstName))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    "Debe ingresar nombre.",
+                    Languages.Aceptar);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.LastName))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    "Debe ingresar apellidos.",
+                    Languages.Aceptar);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    "Debe ingresar un email.",
+                    Languages.Aceptar);
+                return;
+            }
+
+            if (!RegexUtilities.IsValidEmail(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    "Debe ingresar un email valido.",
+                    Languages.Aceptar);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Telephone))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    "Debe ingresar un teléfono.",
+                    Languages.Aceptar);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    "Debe ingresar una contraseña.",
+                    Languages.Aceptar);
+                return;
+            }
+
+            if (this.Password.Length < 6)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    "La contraseña debe tener al menos sies (6) carácteres.",
+                    Languages.Aceptar);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Confirm))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    "Debes ingresar la confirmación de la clave.",
+                    Languages.Aceptar);
+                return;
+            }
+
+            if (this.Password != this.Confirm)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    "La contraseña y la confirmación no concuerdan.",
+                    Languages.Aceptar);
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            var checkConnetion = await this.apiService.CheckConnection();
+            if (!checkConnetion.IsSuccess)
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    checkConnetion.Message,
+                    Languages.Aceptar);
+                return;
+            }
+
+            byte[] imageArray = null;
+            if (this.file != null)
+            {
+                imageArray = FilesHelper.ReadFully(this.file.GetStream());
+            }
+
+            var user = new User
+            {
+                Email = this.Email,
+                FirstName = this.FirstName,
+                LastName = this.LastName,
+                Telephone = this.Telephone,
+                ImageArray = imageArray,
+                UserTypeId = 1,
+                Password = this.Password,
+            };
+
+            var apiSecurity = Application.Current.Resources["APISecurity"].ToString();
+            var response = await this.apiService.Post(
+                apiSecurity,
+                "/api",
+                "/Users",
+                user);
+
+            if (!response.IsSuccess)
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    response.Message,
+                    Languages.Aceptar);
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            await Application.Current.MainPage.DisplayAlert(
+                "Confirmación",
+                "Se realizó correctamente el registro",
+                Languages.Aceptar);
+            Application.Current.MainPage = new MasterPage();
+        }
+
+        public ICommand ChangeImageCommand
+        {
+            get
+            {
+                return new RelayCommand(ChangeImage);
+            }
+        }
+
+        private async void ChangeImage()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (CrossMedia.Current.IsCameraAvailable &&
+                CrossMedia.Current.IsTakePhotoSupported)
+            {
+                var source = await Application.Current.MainPage.DisplayActionSheet(
+                    "¿De donde quiere tomar la imagen?",
+                    "Cancelar",
+                    null,
+                    "Desde la galería",
+                    "Desde la cámara");
+
+                if (source == "Cancelar")
+                {
+                    this.file = null;
+                    return;
+                }
+
+                if (source == "Desde la cámara")
+                {
+                    this.file = await CrossMedia.Current.TakePhotoAsync(
+                        new StoreCameraMediaOptions
+                        {
+                            Directory = "Sample",
+                            Name = "test.jpg",
+                            PhotoSize = PhotoSize.Small,
+                        }
+                    );
+                }
+                else
+                {
+                    this.file = await CrossMedia.Current.PickPhotoAsync();
+                }
+            }
+            else
+            {
+                this.file = await CrossMedia.Current.PickPhotoAsync();
+            }
+
+            if (this.file != null)
+            {
+                this.ImageSource = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    return stream;
+                });
+            }
+        }
+        #endregion
+    
+        #region Comandos
         public ICommand LoginLCommand
         {
             get
             {
                 return new RelayCommand(Login);
             }
-        }
-
-        private async void Register()
-        {
-            MainViewModel.GetInstance().Enter = new EnterViewModel();
-            await Application.Current.MainPage.Navigation.PushAsync(new EnterPage());
         }
 
         private async void Login()
