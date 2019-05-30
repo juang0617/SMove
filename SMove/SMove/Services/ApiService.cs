3ec4eb69ac61efca9f8680dddab7fa47ec1f7a6c
Juan Pablo Garcia Diez <juanp.garciad@upb.edu.co>
2019-05-29 21:04:38 -0500

    Api Email

--- SMove/SMove/Services/ApiService.cs
+++ SMove/SMove/Services/ApiService.cs
@@ -437,5 +437,45 @@ namespace SMove.Services
                 };
             }
         }
+
+        public async Task<User> GetUserByEmail(
+        string urlBase,
+        string servicePrefix,
+        string controller,
+        string email)
+        {
+            try
+            {
+                var model = new UserRequest
+                {
+                    Email = email,
+                };
+
+                var request = JsonConvert.SerializeObject(model);
+                var content = new StringContent(
+                    request,
+                    Encoding.UTF8,
+                    "application/json");
+                var client = new HttpClient
+                {
+                    BaseAddress = new Uri(urlBase)
+                };
+                var url = string.Format("{0}{1}", servicePrefix, controller);
+                var response = await client.PostAsync(url, content);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return null;
+                }
+
+                var result = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<User>(result);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
     }
 }
